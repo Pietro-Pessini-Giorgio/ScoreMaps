c188309880c5410592e19b2cf108716c603a68c9
Andrea <andrea.cozzi2007@gmail.com>
2026-05-25 11:20:46 +0200

    feat: add admin page

---
+++ admin.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pagina admin</title>
+</head>
+<body>
+    <textarea name="textarea" id=""></textarea>
+    <button>text</button>
+    <button type="submit">invia</button>
+</body>
+</html>
