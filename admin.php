83b2270478b431b3fe59912d58284821a303be7c
Stefano Pessini <petergeo6@gmail.com>
2026-05-27 08:58:27 +0200

    feat: add bot rugby maschile

--- admin.php
+++ admin.php
@@ -54,6 +54,23 @@
         }
     }
 
+    if (isset($_POST['use_bot_rm'])) {
+        $scriptPath = __DIR__ . "\\bot\\bot-rgy-m.py";
+        if (!file_exists($scriptPath)) {
+            $sqlOutput = "file non trovato in: " . $scriptPath;
+        } else {
+            $cmd = "python " . escapeshellarg($scriptPath) . " 2>&1";
+            $sqlOutput = shell_exec($cmd);
+
+            if (is_null($sqlOutput)) {
+                $sqlOutput = "ERRORE: shell_exec ha restituito NULL";
+            } elseif (empty($sqlOutput)) {
+                $sqlOutput = "ERRORE: nessun output dallo script";
+            }
+            $sqlOutput = $sqlOutput."\n";
+        }
+    }
+
     if (isset($_POST['invia']) && !empty($_POST['textarea'])) {
         $sql = $_POST['textarea'];
         foreach (explode(";", $sql) as $stmt) {
@@ -79,7 +96,11 @@
     <?php endif; ?>
 
     <form method="POST">
-        <button type="submit" name="use_bot_bm">use bot bascket maschile</button> <button type="submit" name="use_bot_bf">use bot basket femminile</button> <button type="submit" name="use_bot_pm">use bot pallavolo maschile</button> <button type="submit" name="use_bot_pf">use bot pallavolo femminile</button>
+        <button type="submit" name="use_bot_bm">use bot bascket maschile</button> 
+        <button type="submit" name="use_bot_bf">use bot basket femminile</button> 
+        <button type="submit" name="use_bot_pm">use bot pallavolo maschile</button> 
+        <button type="submit" name="use_bot_pf">use bot pallavolo femminile</button>
+        <button type="submit" name="use_bot_rm">use bot rugby maschile</button>
         <br><br>
         <textarea name="textarea" rows="20" cols="80"><?php echo htmlspecialchars($sqlOutput); ?></textarea>
         <br><br>
